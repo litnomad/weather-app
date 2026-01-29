24a321bb430ac58f861f7f4cecc5a92a00c37654
litnomad <128262375+litnomad@users.noreply.github.com>
2026-01-29 17:44:42 -0500

    complete application and dom logic

--- src/dom.js
+++ src/dom.js
@@ -1,8 +1,50 @@
-import { getWeatherData } from "./index";
+import { fetchData, fetchImage } from "./index";
 
 const search = document.querySelector("#location");
 const button = document.querySelector("button");
+const header = document.querySelector("#header");
+const subHeader = document.createElement("div");
+const conditions = document.createElement("h1");
+const temperature = document.createElement("h2");
+const minMax = document.createElement("p");
+const img = document.createElement("img");
+const week = document.querySelector("#week");
 
 button.addEventListener("click", () => {
-  getWeatherData(search.value);
+  fetchData(search.value)
+    .then((data) => {
+      // todays condition
+      conditions.textContent = data.currentConditions.conditions;
+      temperature.textContent = data.currentConditions.temp;
+      minMax.textContent =
+        "Max: " + data.days[0].tempmax + " / " + "Min: " + data.days[0].tempmin;
+
+      header.appendChild(subHeader);
+      subHeader.appendChild(conditions);
+      subHeader.appendChild(temperature);
+      subHeader.appendChild(minMax);
+
+      // current image
+      fetchImage(`${data.currentConditions.conditions}`).then((promise) => {
+        img.classList.add("condition_image");
+        img.src = promise;
+        header.appendChild(img);
+      });
+
+      // week forecast
+      week.innerHTML = "";
+      for (let i = 0; i < 7; i++) {
+        const box = document.createElement("div");
+        const date = document.createElement("p");
+        const dateTemp = document.createElement("p");
+        date.textContent = data.days[i].datetime;
+        dateTemp.textContent = data.days[i].temp;
+        week.appendChild(box);
+        box.appendChild(date);
+        box.appendChild(dateTemp);
+      }
+    })
+    .catch((error) => {
+      console.error("Fail to fetch data", error);
+    });
 });
--- src/index.js
+++ src/index.js
@@ -1,29 +1,64 @@
 import "./dom";
+import "./style.css";
 
-export function getWeatherData(location) {
-  const url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?unitGroup=us&elements=remove%3Acloudcover%2Cremove%3Adew%2Cremove%3Afeelslikemax%2Cremove%3Afeelslikemin%2Cremove%3Aprecipcover%2Cremove%3Apreciptype%2Cremove%3Apressure%2Cremove%3Asolarenergy%2Cremove%3Asolarradiation%2Cremove%3Avisibility&key=EXQLEW6XYD2UMM9HFYJLMLEH6&contentType=json`;
-  fetch(url)
+export function fetchData(location) {
+  return fetch(
+    `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?unitGroup=us&elements=remove%3Acloudcover%2Cremove%3Adew%2Cremove%3Afeelslikemax%2Cremove%3Afeelslikemin%2Cremove%3Aprecipcover%2Cremove%3Apreciptype%2Cremove%3Apressure%2Cremove%3Asolarenergy%2Cremove%3Asolarradiation%2Cremove%3Avisibility&key=EXQLEW6XYD2UMM9HFYJLMLEH6&contentType=json`,
+  )
     .then((response) => {
       if (!response.ok) {
         throw new Error("HTTP error!", response.status);
       }
-      const result = response.json();
-      console.log(result);
-      return result;
+      return response.json();
     })
-    .then((result) => {
-      // current condition, temperature, feels like, hi and lo
+    .then((data) => {
+      console.log(data);
+      // current condition
       console.log(
-        result.currentConditions.conditions,
-        result.currentConditions.temp,
-        result.currentConditions.feelslike,
-        result.days[0].tempmax,
-        result.days[0].tempmin,
+        data.currentConditions.conditions,
+        data.currentConditions.temp,
+        data.currentConditions.feelslike,
+        data.days[0].tempmax,
+        data.days[0].tempmin,
       );
-      // 7 days forecast (temperature)
-      console.log(result.days);
+      // 7 days forecast
+      console.log(
+        data.days[0].datetime,
+        data.days[0].temp,
+        data.days[1].datetime,
+        data.days[1].temp,
+        data.days[2].datetime,
+        data.days[2].temp,
+        data.days[3].datetime,
+        data.days[3].temp,
+        data.days[4].datetime,
+        data.days[4].temp,
+        data.days[5].datetime,
+        data.days[5].temp,
+        data.days[6].datetime,
+        data.days[6].temp,
+      );
+      return data;
     })
     .catch((error) => {
       console.error("Fail to fetch data", error);
+    });
+}
+
+export function fetchImage(condition) {
+  return fetch(
+    `https://api.giphy.com/v1/gifs/translate?api_key=KH2vrIo53DTpqDgTI371yMGIFgZRVvSu&s=${condition}`,
+  )
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("HTTP error!", response.status);
+      }
+      return response.json();
+    })
+    .then((imageData) => {
+      return imageData.data.images.original.url;
+    })
+    .catch((error) => {
+      console.log(error);
     });
 }
